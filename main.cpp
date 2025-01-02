81294e06e9fa725268d6df9ef9e915a4288e69a7
Nikolaj J-K <kagenogmig@gmail.com>
2025-01-02 18:19:04 +0100

    added icon

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QGuiApplication>
+#include <QIcon>
 #include <QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
@@ -13,6 +14,7 @@ int main(int argc, char *argv[])
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
     engine.loadFromModule("mdwa-qt", "Main");
-
+    QIcon icon = QIcon(":/icon.png");
+    app.setWindowIcon(icon);
     return app.exec();
 }
